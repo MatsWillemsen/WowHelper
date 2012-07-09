f7ce98eeaeb730056a91c41441a0a072339a7549
Mats Willemsen <Mats.Willemsen@gmail.com>
2012-07-10 00:07:23 +0200

    Basic multi-client support. Will need to think how that works.

--- wowhelper.py
+++ wowhelper.py
@@ -1,5 +1,8 @@
 import atomac as am
 import time
+import subprocess
+import string
+import re
 from Quartz import *
 from AppKit import NSEvent
 
@@ -7,6 +10,13 @@ from AppKit import NSEvent
 class WowHelper:
 	def __init__(self):
 		self.wow = am.getAppRefByBundleId('com.blizzard.worldofwarcraft')
+	def _getWoWPids(self):
+		pids = {}
+		lines = subprocess.Popen(["/bin/ps","ax","-o","pid,command"], stdout=subprocess.PIPE).communicate()[0]
+		for line in string.split(lines,'\n'):
+			if "Warcraft" in line:
+				pid = re.match("(\d+?)\w+",line).group(0).strip()
+				pids[pid] = line
 	def _waitForMouseMove(self,secs):
 		originalpos = NSEvent.mouseLocation()
 		while True:
@@ -24,3 +34,4 @@ class WowHelper:
 		CGEventPost(kCGHIDEventTap,mouseevent)
 		mouseevent = CGEventCreateMouseEvent(None,kCGEventLeftMouseUp,(x,y),0)
 		CGEventPost(kCGHIDEventTap,mouseevent)
+WowHelper()._getWoWPids()
