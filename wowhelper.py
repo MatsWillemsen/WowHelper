414da51f8e6bd8b682dcbd5c75687b3b1b1defe8
Mats Willemsen <Mats.Willemsen@gmail.com>
2012-07-10 11:55:53 +0200

    Do not execute when the active window is the WoW client. In case you need to restock or something.

--- wowhelper.py
+++ wowhelper.py
@@ -3,12 +3,26 @@ import time
 import subprocess
 import string
 import re
-from Quartz import *
-from AppKit import NSEvent
-from AppKit import NSWorkspace
-	
 
-class WowHelper:
+from Quartz import *
+from AppKit import *
+from Foundation import *
+from PyObjCTools import *
+#from AppKit import NSApplication
+#from AppKit import NSWorkspace
+global latestEvent
+"""
+class AppDelegate(NSObject):
+	def applicationDidFinishLaunching_(self, aNotification):
+		NSEvent.addGlobalMonitorForEventsMatchingMask_handler_(NSKeyDownMask, handler)
+
+def handler(event):
+	NSLog(u"%@",event)
+"""
+class WowHelper():
+	
+	def _handler(self,event):
+		NSLog(u"%@", event)
 	def _getWoWPids(self):
 		pids = []
 		lines = subprocess.Popen(["/bin/ps","ax","-o","pid,command"], stdout=subprocess.PIPE).communicate()[0]
@@ -40,6 +54,11 @@ class WowHelper:
 		mouseevent = CGEventCreateMouseEvent(None,kCGEventLeftMouseUp,(x,y),0)
 		CGEventPost(kCGHIDEventTap,mouseevent)
 	def __init__(self):
+		"""
+		delegate = AppDelegate.alloc().init()
+		NSApplication.sharedApplication().setDelegate_(delegate)
+		AppHelper.runEventLoop()
+		"""
 		pids = self._getWoWPids()
 		print(pids)
 		if(len(pids) != 0):
